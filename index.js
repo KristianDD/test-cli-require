9bd6bb8726fa1092e0e19333f44b4172252a1734
Kristian Dimitrov <kddimitrov@progress.com>
2018-03-13 15:02:56 +0200

    Add constants and getNsConfigDefaultContent

--- index.js
+++ index.js
@@ -9,8 +9,18 @@ const nsconfigJson = fs.readFileSync(".\\demo\\nsconfig.json").toString();
 console.time("cli");
 cli = require(cliPath);
 projectData = cli.projectDataService.getProjectDataFromContent(packageJson, nsconfigJson);
+console.log(cli.constants.CONFIG_NS_FILE_NAME);
+console.log(cli.constants.CONFIG_NS_APP_RESOURCES_ENTRY);
+console.log(cli.constants.CONFIG_NS_APP_ENTRY);
+
+const nsConfData = {};
+nsConfData[cli.constants.CONFIG_NS_APP_ENTRY] = "src";
+console.log(cli.projectDataService.getNsConfigDefaultContent(nsConfData));
+
+console.log(projectData.getNsConfigRelativePath())
 const appPath = projectData.getAppDirectoryRelativePath();
 const resPath = projectData.getAppResourcesRelativeDirectoryPath();
+
 console.timeEnd("cli");
 
 console.log(appPath);
